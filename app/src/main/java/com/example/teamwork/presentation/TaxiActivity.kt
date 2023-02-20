e97842f424f552ea1faec972d07ab139daf5e58d
Azamjon <azamjonostonakulov@gmail.com>
2023-02-21 00:33:29 +0500

    last update

--- app/src/main/java/com/example/teamwork/presentation/TaxiActivity.kt
+++ app/src/main/java/com/example/teamwork/presentation/TaxiActivity.kt
@@ -1,12 +1,14 @@
 package com.example.teamwork.presentation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.teamwork.presentation.taxi_info_fragments.DialogOrderDetailsFragment
 import com.example.teamwork.databinding.ActivityTaxiBinding
+import com.example.teamwork.presentation.taxi_info_fragments.DialogOrderDetailsFragment
 
 class TaxiActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTaxiBinding
+    private var backPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +20,15 @@ class TaxiActivity : AppCompatActivity() {
             val dialogFragment = DialogOrderDetailsFragment()
             dialogFragment.show(supportFragmentManager, "CustomDialog")
         }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            return super.onBackPressed()
+        } else {
+            Toast.makeText(this, "Chiqish uchun yana bosing", Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime = System.currentTimeMillis()
     }
 }
